c612d04293d8d40c197a736a35400811e7422f17
Linda Yeung <lindayeungx@yahoo.com>
2021-01-10 08:36:27 -0500

    Initial Commit

---
+++ Tchibo/TchiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  TchiboApp.swift
+//  Tchibo
+//
+//  Created by Linda Yeung on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TchiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
